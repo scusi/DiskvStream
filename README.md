d294b2f475743214bd2045bd6c76c7524f06fa95
Florian Walther <flw@posteo.de>
2019-07-12 23:39:28 +0200

    initial commit of poc

---
+++ README.md
@@ -0,0 +1,4 @@
+# DiskvStream
+
+The idea is to create a diskv storage that can stream bigdata efficiently,
+without run out of RAM.
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+  "flag"
+  "bufio"
+  "os"
+  "io"
+  "fmt"
+  "log"
+  "path/filepath"
+  "github.com/peterbourgon/diskv"
+)
+
+var (
+  fileName string
+)
+
+func init() {
+  flag.StringVar(&fileName, "f", "", "file to read")
+}
+
+func main() {
+  flag.Parse()
+  s := diskv.New(diskv.Options{
+    BasePath: "data-store",
+  })
+
+  // open the file specified by fileName
+  f, err := os.Open(fileName)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer f.Close()
+
+  rdr := bufio.NewReader(f)
+  key := filepath.Base(fileName)
+// write file to data-store
+  err = s.WriteStream(key, rdr, true)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+// read file from data-storage
+rdrc, err := s.ReadStream(key, true)
+if err != nil {
+  log.Fatal(err)
+}
+out, err := os.Create("outfile")
+if err != nil {
+  log.Fatal(err)
+}
+defer out.Close()
+written, err := io.Copy(out, rdrc)
+if err != nil {
+  log.Fatal(err)
+}
+fmt.Printf("%d bytes written to 'outfile'", written)
+}
